583f14e7e1624ffbdb1f5286d35e8e435a612e03
Yussuf Shariff <yussufshariff@outlook.com>
2023-09-12 00:50:57 +0100

    Basic Nav setup

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,1 +1,37 @@
+import React, { useState } from "react";
+import "../Styles/Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className={`navbar ${isMenuOpen ? "active" : ""}`}>
+      <div className="brand-title"> ✈️ Take Flight </div>
+      <GiHamburgerMenu className="toggle-button" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </GiHamburgerMenu>
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <a href="/Home">Home</a>
+          </li>
+          <li>
+            <a href="/Profile">My Profile</a>
+          </li>
+          <li>
+            <a href="/Bookings">My Bookings</a>
+          </li>
+          <li>
+            <a href="/Contact">Contact us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
